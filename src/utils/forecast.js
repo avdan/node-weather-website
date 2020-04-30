b3c1231856a41feea4e9fd4baeed2437530d9795
dan <dan@danwritescode.com>
2020-04-30 12:53:02 +0800

    changed the forecast strin

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined,`${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees Celsius. It feels like ${body.current.feelslike}.`);
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees Celsius. It feels like ${body.current.feelslike}. The humidity is ${body.current.humidity}, the cloud cover is ${body.current.cloudcover}%. There are ${body.current.precip}% chances of precipitations.`);
         }
 
     })
